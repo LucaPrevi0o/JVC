f21d4a4e3fddc871675e2e1454c26d7a8647b078
LucaPrevi0o <luca01.previati@edu.unife.it>
2024-04-04 16:02:08 +0200

    smth in jvm stopped working

--- jvc/FileParser.java
+++ jvc/FileParser.java
@@ -41,94 +41,54 @@ public class FileParser {
         return null;
     }
 
-    private static void declare(String[] tokens, String dataType, int bitSize) { //declare new signal in list
+    private static void declare(String[] tokens) { //declare new signal in list
 
-        for (var a=1; a<tokens.length-2; a++) { //for every name in declaration list, add new signal
+        var dataType=tokens[tokens.length-1]; //data type
+        if (dataType.startsWith(":")) dataType=dataType.substring(1); //remove separator
+        else if (!tokens[tokens.length-2].endsWith(":")) {
 
-            var s=tokens[a];
-            if (a!=tokens.length-3 && !s.endsWith(",") && !tokens[a+1].startsWith(",")) {
+            System.err.println("Expected declarator before data type");
+            System.exit(1);
+        } else System.out.println(dataType);
+        for (var a=1; a<tokens.length-1; a++) { //for every name in declaration list, add new signal
 
-                System.err.println("Expected separator between declarations");
+            var signal=tokens[a]; //current token
+            System.out.println(a+" - "+signal);
+            if (a>1 && signal.startsWith(",")) signal=signal.substring(1);
+            else if (!tokens[a-1].endsWith(",")) { //no separator before declaration
+
+                System.err.println("Expected separator before declaration");
                 System.exit(1);
-            } else {
-                
-                if (s.endsWith(",")) s=s.substring(0, s.length()-1); //remove separator
-                else if (s.startsWith(",")) s=s.substring(1);
-                if (dataType.equals("bit") || dataType.equals("bit_vector")) signals.add(new Bit(s, bitSize)); //add to declaration list
-                else if (dataType.equals("std_logic") || dataType.equals("std_logic_vector")) signals.add(new StdLogic(s, bitSize)); //add to declaration list
+            } 
+
+            if (a!=tokens.length-2 && signal.endsWith(",")) signal.substring(0, signal.length()-1);
+            else if (!tokens[a+1].startsWith(",")) { //no separator after declaration
+
+                System.err.println("Expected separator after declaration");
+                System.exit(1);
+            } 
+
+            System.out.println(signal);
+            if (!signal.matches("[a-zA-Z][a-zA-Z0-9]+")) { //sanitize signal name
+
+                System.err.println("Illegal signal name");
+                System.exit(1);
             }
         }
     }
 
-    private static void parseTokens(String[] tokens, Signal target, int timeStamp) { //parse divided tokens
+    private void parseDeclaration(String[] tokens) {
 
-        var opPos=new ArrayList<Integer>(); //count number of operations in line
-        for (var a=0; a<tokens.length; a++)
-            if (isOperator(tokens[a])) opPos.add(a);
-
-        var lastTarget=target;
-        for (var a: opPos) { //loop over every nested operation (currently detected as FIFO, every new operation is computed over the result of the previous in line)
-
-            var newTarget=(a.equals(opPos.getLast()) ? target : (target instanceof Bit ?
-                new Bit(target.getName(), target.getDimension()) :
-                new StdLogic(target.getName(), target.getDimension()))); //target for operation
-            var newSource=new Signal[2]; //signals to get data from
-            newSource[0]=(a.equals(opPos.getFirst()) ? getByName(tokens[a-1]) : lastTarget);
-            newSource[1]=getByName(tokens[a+1]);
-            if ((newSource[0]!=null && !newSource[0].getClass().equals(target.getClass()))
-                || (newSource[1]!=null && !newSource[1].getClass().equals(target.getClass()))
-                || (newSource[0]!=null && newSource[1]!=null && !newSource[0].getClass().equals(newSource[1].getClass()))) {
-
-                System.err.println("Operation on type mismatched signals");
-                System.exit(1);
-            }
-            if (a.equals(opPos.getFirst()) && events.size()>0) timeStamp+=events.getLast().getTime(); //delay for event to occur
-            events.add(target instanceof Bit ? 
-                new BitEvent((Bit)newSource[0], (Bit)newSource[1], (Bit)newTarget, tokens[a], timeStamp) :
-                new StdLogicEvent((StdLogic)newSource[0], (StdLogic)newSource[1], (StdLogic)newTarget, tokens[a], timeStamp)); //add event to declaration list
-            lastTarget=newTarget; //save most recent target for last operation in line to be used as source for next operations
-        }
+        
     }
 
     public static void parse(String fileName) { //parse every line of the file
 
         Tokenizer.tokenize(fileName); //tokenize file
         var fileTokens=Tokenizer.getGlobalTokens(); //get tokens from every line
-        for (var lineTokens: fileTokens) {
+        for (var lineToken: fileTokens) { //loop over every line
 
-            var firstToken=lineTokens[0]; //first token (can be eihter declaration or operation assignment)
-            var idToken=lineTokens[lineTokens.length-2];
-            if (firstToken.equals("signal")) {
-
-                var bitSize=lineTokens[lineTokens.length-1]; //bit size for every signal in declaration
-                if (!idToken.endsWith(":")) {
-
-                    System.err.println("Expected bit size identifier");
-                    System.exit(1);
-                } else if (bitSize.equals("bit") || bitSize.equals("std_logic")) declare(lineTokens, bitSize.substring(0, bitSize.length()-1), 1);
-            } else {
-
-                var target=getByName(firstToken);
-                var opToken=lineTokens[1]; //detect token for assignment
-                var lastToken=lineTokens[lineTokens.length-1]; //detect delay for operation chain
-                if (target==null) {
-
-                    System.err.println("Operation with not declared signal");
-                    System.exit(1);
-                } else if (!opToken.equals("<=")) {
-
-                    System.err.println("Expected operation identifier");
-                    System.exit(1);
-                } else if (!idToken.equals("after")) {
-                    
-                    System.err.println("Expected delay identifier");
-                    System.exit(1);
-                } else if (!isNumber(lastToken)) {
-
-                    System.err.println("Expected delay after operation");
-                    System.exit(1);
-                } else parseTokens(lineTokens, target, Integer.parseInt(lastToken)); //parse tokenized line 
-            }
+            if (lineToken[0].equals("signal")) declare(lineToken);
         }
     }
         
