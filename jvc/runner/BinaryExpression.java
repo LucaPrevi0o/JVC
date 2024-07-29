80b6f33b83e834b45d54817bd0fca75595fe437e
LucaPrevi0o <luca01.previati@edu.unife.it>
2024-07-29 11:19:57 +0200

    this is going to be workbench for every release

---
+++ jvc/runner/BinaryExpression.java
@@ -0,0 +1,27 @@
+package jvc.runner;
+
+import java.util.ArrayList;
+import jvc.Signal;
+import jvc.signalType.Type;
+
+public class BinaryExpression extends Expression {
+
+    private Signal<? extends Type> firstOp, secOp;
+
+    public ArrayList<Signal<? extends Type>> getOperands() { 
+        
+        var result=new ArrayList<Signal<? extends Type>>();
+        result.add(firstOp);
+        result.add(secOp);
+        return result;
+    }
+
+    public Signal<? extends Type> execute() { return Signal.execute(firstOp, secOp, opName); }
+
+    public BinaryExpression(Signal<? extends Type> firstOperand, Signal<? extends Type> secondOperand, String operation) {
+
+        firstOp=firstOperand;
+        secOp=secondOperand;
+        opName=operation;
+    }
+}
